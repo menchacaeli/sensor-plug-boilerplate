2602af236b0fd26ecbf1b70474e80313e0f8b055
elimenchaca <menchacaeli@gmail.com>
2018-04-11 14:41:27 -0600

    Added further documentation to all sensor pages and setup page.

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 var express = require('express');
-var app = express();
 var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+// var server = require('http');
+const http = require('http');
+const https = require('https');
+// var server = require('http').createServer(app);
+var io = require('socket.io')(http);
+// var fs = require('fs');
+// var port = process.env.PORT || 3000;
+//
+// server.listen(port, function () {
+//     console.log('Server listening at port %d', port);
+// });
 
-server.listen(port, function () {
-    console.log('Server listening at port %d', port);
-});
+console.log("will serve on http port 8000 and https port 443");
+
+let app = express();
+
+// HTTP server
+http.createServer(app).listen(8000);
+
+// HTTPS server
+// let certificate = fs.readFileSync("./certs/website.crt", 'utf8');
+// let chainLines = fs.readFileSync("./certs/intermediate_domain_ca.crt", 'utf8').split("\n");
+// let cert = [];
+// let ca = [];
+// chainLines.forEach(function(line) {
+//     cert.push(line);
+//     if (line.match(/-END CERTIFICATE-/)) {
+//         ca.push(cert.join("\n"));
+//         cert = [];
+//     }
+// });
+//
+// let httpsOptions = {
+//     key: fs.readFileSync('./certs/privatekey.key'),
+//     cert: fs.readFileSync("/srv/www/keys/chain.pem")
+// };
+//
+// secServer = https.createServer(httpsOptions, app);
+// secServer.listen(8080);
 
 // Routing
 app.use(express.static(path.join(__dirname, '/node_modules')));
